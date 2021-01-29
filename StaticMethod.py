37e1bcce5fd8030d1861afa8ed3f756c1dd4627a
ksch535 <kschmidt@ok.de>
2021-01-29 11:32:12 +0100

    Update StaticMethod.py

--- StaticMethod.py
+++ StaticMethod.py
@@ -1,6 +1,6 @@
 # v 1.0
 
-# for "making" static methods
+# for "making" static methods, example inside of class declarations: method = StaticMethod(method)
 class StaticMethod:
     def __init__(self, anycallable):
         self.__call__ = anycallable
